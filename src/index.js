93e4cbef549c3e8a5718bd71d2a45bac35d65bae
yonatana <yonatana@jfrog.com>
2017-05-23 18:21:42 +0300

    middleware

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import {Provider} from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import {BrowserRouter, Route} from 'react-router-dom';
 import reducers from './reducers';
-import promise from 'redux-promise';
+import ReduxPromise from 'redux-promise';
 import PostsIndex from './components/posts_index';
 
-const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 
 ReactDOM.render(
