cedc59b116412d1ddb1b75c75951f4d88a411d02
yonarbel <yonarbel@gmail.com>
2017-05-28 01:01:22 +0300

    formapp

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,51 @@
 import React, {Component, PropTypes} from 'react';
 import {connect} from 'react-redux';
+import {Field, reduxForm} from 'redux-form';
+class PostsNew extends Component {
 
-export default class PostsNew extends Component {
-  render() {
+  renderField(field) {
+
     return (
-      <div>Posts New !</div>
+
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input}/>
+        {field.meta.error}
+      </div>
+    )
+  }
+  onSubmit(values){
+    console.log('form vals are',values)
+  }
+  render() {
+    const {handleSubmit} = this.props;
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field label="Title" name="title" component={this.renderField}/>
+        <Field label="Category" name="category" component={this.renderField}/>
+        <Field label="Post Content" name="content" component={this.renderField}/>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-PostsNew.propTypes = {};
+function validate(val){
+const errors = {};
 
-// connect(,)(PostsNew);
+if(!val.title)
+errors.title = "Please enter title";
+if(!val.category)
+errors.category = "Please enter category";
+if(!val.content)
+errors.content = "Please enter content";
+
+return errors;
+}
+
+export default reduxForm({
+  form: 'PostsNewForm',
+  validate
+
+
+})(PostsNew);
