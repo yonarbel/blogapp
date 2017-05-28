20f55f807e2e18e4461d63197e4478dae5419289
yonatana <yonatana@jfrog.com>
2017-05-28 19:19:44 +0300

    new ui

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,16 @@
 import axios from 'axios';
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_LINTERS = 'fetch_linters';
-	const ROOT_URL = "http://reduxblog.herokuapp.com/api";
-	const API_KEY = '?key=YONATANARBEL1234'
+const ROOT_URL = "http://reduxblog.herokuapp.com/api";
+const API_KEY = '?key=YONATANARBEL1234'
 
 
 /*export function fetchPosts(linter) {
-	const request = axios.get(`${ROOT_URL}/posts/${API_KEY}`);
-	return {
-		type: FETCH_POSTS,
-		payload : request
-	}
+ const request = axios.get(`${ROOT_URL}/posts/${API_KEY}`);
+ return {
+ type: FETCH_POSTS,
+ payload : request
+ }
 
 
  }*/
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -2,12 +2,12 @@ import {FETCH_POSTS} from '../actions';
 import {FETCH_LINTERS} from '../actions';
 import _ from 'lodash';
 
-export default function(state = {},action){
-	switch(action.type){
-		case FETCH_POSTS:
+export default function (state = {}, action) {
+    switch (action.type) {
+        case FETCH_POSTS:
             return action.payload.data;
-		default :
-		return state
+        default :
+            return state
 
-	}
+    }
 }
